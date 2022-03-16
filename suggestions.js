5e56d1319cf4b68bc970c2c179752439d6ff2c9c
Blu0009 <86554204+Blu0009@users.noreply.github.com>
2022-03-16 20:18:27 +0100

    Add files via upload

---
+++ suggestions.js
@@ -0,0 +1,12 @@
+let suggestions = [
+    "Discord",
+    "Coding",
+    "Twitch",
+    "YouTube",
+    "Instagram",
+    "Facebook (nie polecam)",
+    "Twitter",
+    "Inferno Flames",
+    "Facebook",
+    "Memy",
+];
